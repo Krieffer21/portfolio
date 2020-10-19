a82f64939a6b77481acc0b0fa9e3306e263bccfe
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-18 22:04:14 -0700

    fixing img path

--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import Image from "react-bootstrap/Image";
 
 function Portfolio(props) {
     return (
         <div className="portfolio col-8 mx-auto mt-5 mb-3">
             <div className="port card ">
-                <img className="project" alt={props.alt} src={props.src} />
+                <Image className="project" alt={props.alt} src={props.src}></Image> 
                 <div className="card-body ">
                     <h5 className="card-title">{props.project}</h5>
                     <p className="card-text aboutpro">{props.about}</p>
