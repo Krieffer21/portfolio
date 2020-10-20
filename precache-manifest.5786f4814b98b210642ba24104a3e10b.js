97b6390452b39f277d1db4dde8118578f5e94a51
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-19 21:32:56 -0700

    Updates

---
+++ precache-manifest.5786f4814b98b210642ba24104a3e10b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d833936993b9836cd12ec9a1507daa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "238270195ba03054a5eb",
+    "url": "/portfolio/static/css/main.02808acd.chunk.css"
+  },
+  {
+    "revision": "cf0be2e2b53b455c8c4b",
+    "url": "/portfolio/static/js/2.aac8c8a5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.aac8c8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238270195ba03054a5eb",
+    "url": "/portfolio/static/js/main.cd26162e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
